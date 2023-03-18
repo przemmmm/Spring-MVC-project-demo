ab40cfc7931d438b5a227afc03dc4ae625ab67dc
Przemek Koscinski <przem.koscinski@gmail.com>
2023-03-18 11:32:25 +0100

    initial

---
+++ src/main/java/com/mvc/SpringMvcProjectDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcProjectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcProjectDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvc/SpringMvcProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
